98d0ed97f1743bc128641282d333bdc6768229e5
Sven Over <sven@cord.com>
2022-06-17 22:54:21 +0100

    [𝘀𝗽𝗿] 𝘪𝘯𝘪𝘵𝘪𝘢𝘭 𝘷𝘦𝘳𝘴𝘪𝘰𝘯
    Created using spr 1.3.3-beta.1

--- src/commands/diff.rs
+++ src/commands/diff.rs
@@ -373,11 +373,15 @@ async fn diff_impl(
 
         Some(git.create_derived_commit(
             local_commit.parent_oid,
-            if pull_request.is_some() {
-                "[𝘀𝗽𝗿] 𝘤𝘩𝘢𝘯𝘨𝘦𝘴 𝘪𝘯𝘵𝘳𝘰𝘥𝘶𝘤𝘦𝘥 𝘵𝘩𝘳𝘰𝘶𝘨𝘩 𝘳𝘦𝘣𝘢𝘴𝘦\n\n[skip ci]"
-            } else {
-                "[𝘀𝗽𝗿] 𝘤𝘩𝘢𝘯𝘨𝘦𝘴 𝘵𝘰 𝘮𝘢𝘴𝘵𝘦𝘳 𝘵𝘩𝘪𝘴 𝘤𝘰𝘮𝘮𝘪𝘵 𝘪𝘴 𝘣𝘢𝘴𝘦𝘥 𝘰𝘯\n\n[skip ci]"
-            },
+            &format!(
+                "{}\n\nCreated using spr {}\n\n[skip ci]",
+                if pull_request.is_some() {
+                    "[𝘀𝗽𝗿] 𝘤𝘩𝘢𝘯𝘨𝘦𝘴 𝘪𝘯𝘵𝘳𝘰𝘥𝘶𝘤𝘦𝘥 𝘵𝘩𝘳𝘰𝘶𝘨𝘩 𝘳𝘦𝘣𝘢𝘴𝘦"
+                } else {
+                    "[𝘀𝗽𝗿] 𝘤𝘩𝘢𝘯𝘨𝘦𝘴 𝘵𝘰 𝘮𝘢𝘴𝘵𝘦𝘳 𝘵𝘩𝘪𝘴 𝘤𝘰𝘮𝘮𝘪𝘵 𝘪𝘴 𝘣𝘢𝘴𝘦𝘥 𝘰𝘯"
+                },
+                env!("CARGO_PKG_VERSION"),
+            ),
             new_base_tree,
             &parents[..],
         )?)
@@ -424,10 +428,14 @@ async fn diff_impl(
     // Create the new commit
     let pr_commit = git.create_derived_commit(
         local_commit.oid,
-        github_commit_message
-            .as_ref()
-            .map(|s| &s[..])
-            .unwrap_or("[𝘀𝗽𝗿] 𝘪𝘯𝘪𝘵𝘪𝘢𝘭 𝘷𝘦𝘳𝘴𝘪𝘰𝘯"),
+        &format!(
+            "{}\n\nCreated using spr {}",
+            github_commit_message
+                .as_ref()
+                .map(|s| &s[..])
+                .unwrap_or("[𝘀𝗽𝗿] 𝘪𝘯𝘪𝘵𝘪𝘢𝘭 𝘷𝘦𝘳𝘴𝘪𝘰𝘯"),
+            env!("CARGO_PKG_VERSION"),
+        ),
         new_head_tree,
         &pr_commit_parents[..],
     )?;
