92c0ffa666eeea1a98b6f27e120fcaac74eeb820
Sven Over <sven@svenover.de>
2026-01-22 22:54:15 +0000

    Remove license headers and references to Radical HQ

--- src/message.rs
+++ src/message.rs
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) Radical HQ Limited
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 use color_eyre::eyre::{Result, bail};
 
 use crate::output::output;
